3881f6345f7ae147ec37ae7f4f759a297e640c27
Flacko <lesliejoe2k2@gmail.com>
2025-04-06 06:59:04 +0000

    fixed title typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Benevo Waitllist",
+  title: "Benevo Waitlist",
   description: "Join Our Mail List For Updates",
 };
 
