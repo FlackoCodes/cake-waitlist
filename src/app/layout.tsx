8c59cddd1169c591b0289363f5163684274122d2
Joseph Kofi Leslie Lartey <lesliejoe2k2@gmail.com>
2025-10-02 11:11:00 -0700

    updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Benevo Waitlist",
+  title: "Primal Waitlist",
   description: "Join Our Mail List For Updates",
 };
 
